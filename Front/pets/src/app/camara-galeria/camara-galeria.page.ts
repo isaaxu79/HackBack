ee00343c236aeaa901c0513f9967b3282aee5468
Isaac A. Marin <VasMar_ia@hotmail.com>
2019-10-26 03:05:10 -0500

    camera

--- Front/pets/src/app/camara-galeria/camara-galeria.page.ts
+++ Front/pets/src/app/camara-galeria/camara-galeria.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ImagePicker } from '@ionic-native/image-picker/ngx';
 
 @Component({
   selector: 'app-camara-galeria',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CamaraGaleriaPage implements OnInit {
 
-  constructor() { }
+  constructor(private imagePicker: ImagePicker) { }
 
   ngOnInit() {
   }
 
+
+  async openGalery(){
+    handler: () => {
+      let options = { maximumImagesCount: 1, 
+        quality:100
+      }
+      this.imagePicker.getPictures(options).then((results) => {
+          console.log(results[0])
+          let arr = results[0].split('/')
+          let directory = arr[0]
+          for(let m=1;m<arr.length-1;m++)
+            directory = directory + "/"+ arr[m] 
+          console.log(directory)
+      }, (err) => { 
+        console.log("error en takepickture")
+        console.log(err)});
+
+    }
+  }
 }
