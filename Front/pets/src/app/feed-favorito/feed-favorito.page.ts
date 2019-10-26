c43391e1ae7806f7fb999713bc7f9ccde6b36d52
unknown <173220@ids.upchiapas.edu.mx>
2019-10-26 16:05:51 -0500

    feed-adopcion y feed-match

--- Front/pets/src/app/feed-favorito/feed-favorito.page.ts
+++ Front/pets/src/app/feed-favorito/feed-favorito.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RestApiService } from '../rest-api.service';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-feed-favorito',
   templateUrl: './feed-favorito.page.html',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedFavoritoPage implements OnInit {
 
-  constructor() { }
+  items: any;
+  constructor(private api: RestApiService,public toastController: ToastController) { }
 
+
+  estrellaAmarilla="md-star";
+  estrella="star-outline";
+  color = this.estrella;
   ngOnInit() {
+    this.getInfo();
   }
+  datos = {
+    "tipo":"favoritos",
+    "campo":"especie",
+    "valor":"Lomo"
+  }
+
+  buttonColor = "FEDC1E";
+
+  async getInfo(){
+    await this.api.postDataLocal(this.datos, "api/v1/publicacion/filter")
+    .subscribe(res => {
+        this.items=res.publicacion;
+        this.presentToast(res.message);
+    },(err) => {
+      console.log(err);
+      this.presentToast("Ocurrió un error interno");
+    });
+  }
+
+  async presentToast(message) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  favoritos(item){
+    if(item.star = "#FFFFFF"){
+      item.star = "#FEDC1E"
+    }else{
+      item.star = "#FFFFFF";
+    }
+  }
+
 
 }
