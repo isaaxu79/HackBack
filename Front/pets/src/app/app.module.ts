e2a0932882db59f9cbefc6076b011f1010a73504
Isaac A. Marin <VasMar_ia@hotmail.com>
2019-10-26 03:02:17 -0500

    firebasePhotos

--- Front/pets/src/app/app.module.ts
+++ Front/pets/src/app/app.module.ts
@@ -9,10 +9,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { firebaseConfig } from '../environments/firebaseconfig';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireStorageModule, AngularFirestoreModule],
   providers: [
     StatusBar,
     SplashScreen,
