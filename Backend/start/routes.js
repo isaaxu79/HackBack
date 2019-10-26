76a20a257fa6b357ae21d8cfbb276e29be7e5540
Isaac A. Marin <VasMar_ia@hotmail.com>
2019-10-26 01:54:24 -0500

    APIv1

--- Backend/start/routes.js
+++ Backend/start/routes.js
@@ -9,3 +9,26 @@ Route.get('/', () => {
 
 Route.post('/auth/register', 'AuthController.register')
 Route.post('/auth/login', 'AuthController.login')
+
+
+Route.group(() => {
+  Route.resource('empresa', 'EmpresaController').apiOnly()
+  Route.resource('comun', 'ComunController').apiOnly()
+  Route.resource('mascota', 'MascotaController').apiOnly()
+  Route.resource('publicacion', 'EmpresaController').apiOnly()
+  Route.resource('foto_empresa', 'FotoEmpresaController').apiOnly()
+  Route.resource('foto_publicacion', 'FotoPublicacionController').apiOnly()
+  Route.resource('favoritos', 'FavoritoController').apiOnly()
+
+  Route.get('mascotas/mypets/:id','MascotaController.getByUser')
+  Route.get('mascotas/pets','MascotaController.getByName')
+  Route.get('comun/user/:id','ComunController.getByUser')
+  Route.get('empresa/user/:id','EmpresaController.getByUser')
+  Route.get('favorito/user/:id','FavoritoController.getByUser')
+  Route.get('foto_empresa/user/:id','FotoEmpresaController.getByUser')
+  Route.get('foto_empresa/user/:id','FotoPublicacionController.getByUser')
+  Route.get('publicacion/user/:id','PublicacionController.getByUser')
+
+  Route.delete('/delete/:id', 'AuthController.destroy')
+
+}).prefix('api/v1').middleware('auth')
