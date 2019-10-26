45f087d2accb7ed584c9fd6508e228fc67f253a1
Edddduardo <173631@ids.upchiapas.edu.mx>
2019-10-26 03:15:03 -0500

    Vista login

--- Front/pets/src/app/app-routing.module.ts
+++ Front/pets/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'camara-galeria', loadChildren: './camara-galeria/camara-galeria.module#CamaraGaleriaPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
 ];
 
 @NgModule({
