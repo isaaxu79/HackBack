6e375c8cc9e726bd7040e2217fafc9c1348bd243
Freddy Solis <173239@ids.upchiapas.edu.mx>
2019-10-27 07:26:56 -0600

    Perfil

--- Front/pets/src/app/profile/profile.page.ts
+++ Front/pets/src/app/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RestApiService } from '../rest-api.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
-
+  constructor(public api: RestApiService,public toastController: ToastController) { }
+  items:any
+  ready=false
   ngOnInit() {
   }
+  async getAllData(){
+    let uid = window.localStorage.getItem("idUser");
 
+    await this.api.getDataLocal("api/v1/comun/user/"+uid)
+    .subscribe(res => {
+        this.items=res[0];
+        console.log(this.items)
+        this.ready = true
+    },(err) => {
+      console.log(err);
+      this.presentToast("Ocurrió un error interno");
+    });
+    
+  }
+
+  async presentToast(message) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  ionViewWillEnter(){
+    this.getAllData()
+  }
 }
