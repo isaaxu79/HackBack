63c350809d57bc34a5d9acccc3649e2577b6a7ba
Ernesto-Guerra <173216@ids.upchiapas.edu.mx>
2019-10-26 20:30:44 -0500

    ee

--- Front/pets/src/app/app-routing.module.ts
+++ Front/pets/src/app/app-routing.module.ts
@@ -17,16 +17,6 @@ const routes: Routes = [
 
 ];
 
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
-
-
-];
 
 @NgModule({
   imports: [
